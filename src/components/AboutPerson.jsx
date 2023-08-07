24af9b1d358b917b8c766cdc3acf8a09386fbe2a
hayzedd2 <azeezalhameen1@gmail.com>
2023-08-07 09:30:48 +0100

    using components and props in react

---
+++ src/components/AboutPerson.jsx
@@ -0,0 +1,14 @@
+import Person from "../images/person.jpg"
+
+
+
+const AboutPerson = ()=>{
+    return (
+        <div className="container">
+            <img src={Person} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores tempore labore eligendi, quo unde autem praesentium quisquam quibusdam quidem quas, ex deserunt quasi nihil fugiat quod? Commodi quod voluptate eos?</p>
+        </div>
+    )
+}
+
+export default AboutPerson
---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Skills(){
+    return (
+        <div className="skills-container">
+            <h1 className="skills-title">Skills</h1>
+            <ul>
+                <li>Mobile Developer</li>
+                <li>Backend Developer</li>
+                <li>Frontend Developer</li>
+            </ul>
+        </div>
+    )
+}
